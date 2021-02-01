483aa3e66a90457185a39c6d4d45641d2abe0ffc
AymericLaissus <57354584+AymericLaissus@users.noreply.github.com>
2021-02-01 17:07:17 +0100

    Delete fusionlistetri.scm

--- fusionlistetri.scm
+++
@@ -1,6 +0,0 @@
-(define fusltri 
-    (lambda (L1 L2)
-        (cond ((null? L1) L2)
-            ((null? L2) L1)
-            ((< (car L1) (car L2)) (cons (car L1) (fusltri (cdr L1) L2)))
-            (else (cons (car L2) (fusltri (cdr L2) L1))))))
